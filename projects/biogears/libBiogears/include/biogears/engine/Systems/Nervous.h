ik1994/core

feat:mmcdaniel-baroreceptors:Add test functions for autoregulation
Define local oxygen feedback and cerebral autoregulation effects.
Engine stabilizes in ~280s for standard run.  Feedback during hemorrhage
looks reasonable.

projects/biogears/libBiogears/include/biogears/engine/Systems/Nervous.h
@@ -75,6 +75,8 @@ protected:
   void SympatheticSignalProcess();
   void ParasympatheticSignalProcess();
   void EfferentResponse();
+  void LocalRegulation();
+  void CerebralAutoregulation();
 
 
   double m_FilteredPressure;
@@ -123,6 +125,18 @@ protected:
   std::vector<double> m_HeartRateEffectors;
   std::vector<double> m_HeartElastanceEffectors;
   std::vector<double> m_ResistanceEffectors;
+
+  double m_LocalMuscleO2Effect;
+  double m_LocalHeartO2Effect;
+  double m_LocalMuscleO2Baseline;
+  double m_LocalHeartO2Baseline;
+  double m_CerebralAutoEffect;
+  double m_HypoxiaEffectPeripheral;
+  double m_HypoxiaEffectNode;
+  double m_CerebralBloodFlowBaseline_mL_Per_s;
+  double m_CerebralCO2Baseline_mmHg;
+  double m_CerebralBloodFlowFilter;
+
 
 
   bool m_TestBaroreceptors;

ardysmoker99/core-1

projects/biogears/libBiogears/include/biogears/engine/Systems/Nervous.h
@@ -70,34 +70,24 @@ protected:
   void ChemoreceptorFeedback();
   void SetPupilEffects();
 
-  void UrsinoBaroreceptor();
-
   void AfferentResponse();
-  void SympatheticSignalProcess();
-  void ParasympatheticSignalProcess();
+  void CentralSignalProcess();
   void EfferentResponse();
+  void CerebralAutoregulation();
 
 
-  double m_FilteredPressure;
-  double m_HeartRateModSympathetic;
-  double m_HeartRateModVagal;
-  double m_ResistanceMod;
-  double m_ElastanceMod;
-  double m_ComplianceMod;
+
   // Serializable member variables (Set in Initialize and in schema)
   double m_AfferentChemoreceptor_Hz;
   double m_ArterialOxygenBaseline_mmHg;
   double m_ArterialCarbonDioxideBaseline_mmHg;
   double m_CerebralCarbonDioxideSetPoint_mmHg;
   double m_BaroreceptorFatigueScale;
-
-  double m_BaroreceptorFrequencyBaseline_Hz;
   double m_CentralVentilationDelta_L_Per_min;
   double m_ChemoreceptorFiringRate_Hz;
   double m_ChemoreceptorFiringRateSetPoint_Hz;
   double m_PeripheralBloodGasInteractionBaseline_Hz;
   double m_PeripheralVentilationDelta_L_Per_min;
-  double m_PreviousMeanArterialPressure_mmHg;
 
 
   // Stateless member variable (Set in SetUp())
@@ -114,21 +104,34 @@ protected:
   double m_painStimulus;
 
   //Testing combined signals
-  double m_AfferentBaroreceptor_Hz;
-  double m_AfferentPulmonary_Hz;	//*AP
-  double m_AfferentAtrial_Hz;		//*AA
+  double m_AfferentBaroreceptor;
+  double m_AfferentPulmonary;	//*AP
+  double m_AfferentAtrial;		//*AA
   double m_FilteredPulmonaryVenousPressure_Hz; //*AA
   double m_AfferentThermal_Hz;		//*AT
-  double m_SympathethicNode_Hz;
-  double m_SympatheticPeripheral_Hz;
-  double m_Vagal_Hz;
-  double m_AchVagal;
-  double m_AchSympatheticNode;
-  double m_AchSympatheticPeripheral;
-  double m_AdrenalMedulla;
+  double m_SympatheticSignal;
+  double m_VagalSignal;
+  double m_PressureHalfMax;
+  double m_AfferentStrain;
+
+  double m_HeartRateModSympathetic;
+  double m_HeartRateModVagal;
+  double m_HeartRateModRespiratory;
+  double m_ResistanceMod;
+  double m_ElastanceMod;
+  double m_ComplianceMod;
+
   std::vector<double> m_HeartRateEffectors;
-  std::vector<double> m_HeartElastanceEffectors;
+  //std::vector<double> m_HeartElastanceEffectors;
   std::vector<double> m_ResistanceEffectors;
+  double m_HeartElastanceEffector;
+
+
+  double m_CerebralAutoEffect;
+  double m_CerebralBloodFlowBaseline_mL_Per_s;
+  double m_CerebralCO2Baseline_mmHg;
+  double m_CerebralBloodFlowFilter;
+
 
 
   bool m_TestBaroreceptors;