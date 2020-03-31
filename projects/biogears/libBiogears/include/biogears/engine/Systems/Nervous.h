ik1994/core

f/mmcdaniel-baroreceptors: Generate combined signal w/ new model
Introduce the new baroreceptor signal to the combined model.  Parameters
tuned to generate outputs consistent with Ursino and within approximate
BioGears validation for hemorrhage scenarios.  Need to test hypoxia and
hypercapnia.

projects/biogears/libBiogears/include/biogears/engine/Systems/Nervous.h
@@ -104,7 +104,8 @@ protected:
   double m_AfferentAtrial;		//*AA
   double m_FilteredPulmonaryVenousPressure_Hz; //*AA
   double m_AfferentThermal_Hz;		//*AT
-  double m_SympatheticSignal;
+  double m_SympatheticHeartSignal;
+  double m_SympatheticPeripheralSignal;
   double m_VagalSignal;
   double m_PressureHalfMax;
   double m_AfferentStrain;
@@ -116,9 +117,15 @@ protected:
   double m_ElastanceMod;
   double m_ComplianceMod;
 
-  std::vector<double> m_HeartRateEffectors;
-  //std::vector<double> m_HeartElastanceEffectors;
-  std::vector<double> m_ResistanceEffectors;
+  //Baselines
+  double m_BaroreceptorBaseline;
+  double m_SympatheticHeartSignal_Baseline;
+  double m_SympatheticPeripheralSignal_Baseline;
+  double m_VagalSignal_Baseline;
+
+
+
+
   double m_HeartElastanceEffector;
 
 

ardysmoker99/core-1

projects/biogears/libBiogears/include/biogears/engine/Systems/Nervous.h
@@ -75,22 +75,44 @@ protected:
   void EfferentResponse();
   void CerebralAutoregulation();
 
-
+   bool m_TestBaroreceptors; //For testing
 
   // Serializable member variables (Set in Initialize and in schema)
   double m_AfferentChemoreceptor_Hz;
   double m_ArterialOxygenBaseline_mmHg;
   double m_ArterialCarbonDioxideBaseline_mmHg;
-  double m_CerebralCarbonDioxideSetPoint_mmHg;
+  double m_AfferentPulmonaryStretchReceptor_Hz;
+  double m_AfferentStrain;
+  double m_AfferentStrainBaseline;
+  double m_ArterialOxygenSetPoint_mmHg; /// \todo Aaron these should be baselines.
+  double m_ArterialCarbonDioxideSetPoint_mmHg;
+  double m_BaroreceptorBaseline;
   double m_BaroreceptorFatigueScale;
+  double m_BaroreceptorOperatingPoint_mmHg;
   double m_CentralVentilationDelta_L_Per_min;
-  double m_ChemoreceptorFiringRate_Hz;
   double m_ChemoreceptorFiringRateSetPoint_Hz;
+  double m_CerebralAutoregulator;
+  double m_CerebralBloodFlowBaseline_mL_Per_s;
+  double m_CerebralBloodFlowInput_mL_Per_s;
+  double m_CerebralCarbonDioxideBaseline_mmHg;
+  double m_ComplianceModifier;
+  double m_HeartElastanceModifier;
+  double m_HypoxiaThresholdHeart;
+  double m_HypoxiaThresholdPeripheral;
+  double m_ResistanceModifier;
   double m_PeripheralBloodGasInteractionBaseline_Hz;
   double m_PeripheralVentilationDelta_L_Per_min;
+  double m_PulmonaryVenousPressureBaseline_mmHg;
+  double m_PulmonaryVenousPressureInput_mmHg;
+  double m_SympatheticHeartSignalBaseline;
+  double m_SympatheticPeripheralSignalBaseline;
+  double m_VagalSignalBaseline;
 
 
   // Stateless member variable (Set in SetUp())
+  double m_AfferentBaroreceptor_Hz;
+  double m_AfferentAtrial_Hz; //*AA
+  double m_AfferentThermal_Hz; //*AT
   bool m_FeedbackActive;
   double m_dt_s;
   SESubstance* m_Succinylcholine;
@@ -102,39 +124,9 @@ protected:
   double m_painVASDuration_s;
   double m_painVAS;
   double m_painStimulus;
-
-  //Testing combined signals
-  double m_AfferentBaroreceptor;
-  double m_AfferentPulmonary;	//*AP
-  double m_AfferentAtrial;		//*AA
-  double m_FilteredPulmonaryVenousPressure_Hz; //*AA
-  double m_AfferentThermal_Hz;		//*AT
-  double m_SympatheticSignal;
-  double m_VagalSignal;
-  double m_PressureHalfMax;
-  double m_AfferentStrain;
-
-  double m_HeartRateModSympathetic;
-  double m_HeartRateModVagal;
-  double m_HeartRateModRespiratory;
-  double m_ResistanceMod;
-  double m_ElastanceMod;
-  double m_ComplianceMod;
-
-  std::vector<double> m_HeartRateEffectors;
-  //std::vector<double> m_HeartElastanceEffectors;
-  std::vector<double> m_ResistanceEffectors;
-  double m_HeartElastanceEffector;
-
-
-  double m_CerebralAutoEffect;
-  double m_CerebralBloodFlowBaseline_mL_Per_s;
-  double m_CerebralCO2Baseline_mmHg;
-  double m_CerebralBloodFlowFilter;
-
-
-
-  bool m_TestBaroreceptors;
+  double m_SympatheticHeartSignal_Hz;
+  double m_SympatheticPeripheralSignal_Hz;
+  double m_VagalSignal_Hz;
 
   // Configuration fractions representing the amount by which each quantity can change due to baroreceptors;
   double m_normalizedGammaHeartRate;